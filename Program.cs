01b15771904f278f34ea1bfdd916adecfd808c85
TechnikNolife <jakub.luckiewicz1@gmail.com>
2022-01-09 13:06:03 +0100

    Add new files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wiek = 0;
+            Console.WriteLine("Podaj swój wiek");
+            wiek = Convert.ToInt32(Console.ReadLine());
+            
+            if(wiek<15)
+            {
+                Console.WriteLine("Leksiu to lubi :0");
+            }
+            else
+            {
+                Console.WriteLine("Nie no, idziesz do domu");
+            }
+        }
+    }
+}
